617f3c3143f36b15c504f82d14ae441aea24e8d5
Xiao Yu Wnag <40452784+Ohpaipai@users.noreply.github.com>
2018-11-04 22:44:40 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,14 +1,11 @@
 #include"subroutine.h"
-
 int main(int argc, char *argv[])
 {
-
-	
 	//ifile.open(argv[1], ios::in );//inputfile cmd
 	//ofile.open(argv[2], ios::out);//outputfile cmd
 	ifile.open("input.pla", ios::in);//input test
-	ofile.open("output.dot", ios::out);//output test
-	while (getline(ifile, readfilestring))//read a line once again
+	//ofile.open("output.dot", ios::out);//output test
+	while (getline(ifile,readfilestring))//read a line once again
 	{
 		if (IsP)
 		{
@@ -20,9 +17,27 @@ int main(int argc, char *argv[])
 			readmyfileoutofP();//process my file
 		if (Ise) break;//the end of the file
 	}
+	head = 0;
+	tail = pow(2,mytruth.gethowmanyin());
+	int jt = 0;
+	for (int i = 0; i < tail; i++)
+	{
+		if (mytruth.getmiofout(i) == 1 || mytruth.getmiofout(i) == -1)
+		{
+			//cout << i << endl;
+			con[jt] = tob(i);
+			//cout << con[jt] << endl;
+			jt++;
+		}
+	}
 	
+	tail = jt;
+	countofmc = tail;
+	head = 0;
+	mcfinal.clear();
+	McCluskeyalgorithm(mytruth.gethowmanyin());
 	ifile.close();
-	ofile.close();
-	system("pause");
+	//ofile.close();
+	system("PAUSE");
 	return 0;
 }
